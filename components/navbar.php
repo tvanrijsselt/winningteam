702c6017ae469ec56ffd30792189917cf47f5dbd
FemkeLeentje <femkelb@gmail.com>
2018-04-19 15:48:38 +0200

    started on navbar

---
+++ components/navbar.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<!-- to do:
+    - styling
+    - logout
+    - user -> ingelogde user
+    - werkende links
+-->
+
+<html>
+    <head>
+    </head>
+        <style>
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                border-bottom: black solid 1px;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                background-color: white;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                padding: 8px;
+                text-decoration: none;
+                color: black;
+            }
+
+            .dropdown {
+                display: none;
+                position: absolute;
+                right: 0;
+                z-index: 1;
+            }
+
+            .user:hover .dropdown {
+                display: block;
+            }
+
+            .dropdown a:hover {
+                background-color: green;
+            }
+        </style>
+
+    <body>
+        <ul class='navbar'>
+            <li><a href="#">Twitter</a></li>
+            <li class='user' style="float: right">
+                <a href="#">User</a>
+                <div class='dropdown'>
+                    <a href="#">Account</a>
+                    <a href="#">Logout</a>
+                </div>
+                </li>
+        </ul>
+    </body>
+
+
+</html>
