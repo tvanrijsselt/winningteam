bc646aa4b105a7fe50223a48985ffeee03e90a03
Tim <tim.lorent@icloud.com>
2018-05-02 11:35:39 +0200

    Added security measures for input fields.

--- components/process_reply.php
+++ components/process_reply.php
@@ -8,6 +8,9 @@ $picture = $_POST['picture'];
 $userid = $_SESSION['user-id']; //change to logged in userid
 $tweet_id = $_POST['tweet_id'];
 
+$content = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['content']))));
+$content = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['picture']))));
+
 if ($content != '' OR $picture != '') {
     run_mysql_query("INSERT INTO replies (content, picture, user_id, tweet_id, created_at) 
                  VALUES ('{$content}', '{$picture}', '{$userid}', '{$tweet_id}', now())");
--- components/tweet-account-actions.php
+++ components/tweet-account-actions.php
@@ -27,14 +27,17 @@
   // Edit a tweet in the account page.
   if(isset($_POST['action']) && $_POST['action'] == 'edit_tweet_hidden') {
 
-    $editTweetPhoto = $_POST['edit_tweet-photo'];
-    $editTweetText = $_POST['edit_tweet'];
+    // $editTweetPhoto = $_POST['edit_tweet-photo'];
+    $editTweetPhoto = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['edit_tweet-photo']))));
+    // $editTweetText = $_POST['edit_tweet'];
+    $editTweetText = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['edit_tweet']))));
 
     if(!empty($_POST['edit_tweet_submit'])) {
       if(empty($editTweetText) && empty($editTweetPhoto)) {
         header("Location: ./account.php");
       }
       elseif(empty($editTweetPhoto) && !empty($editTweetText)) {
+        
         $query = "UPDATE tweets SET tweet = '$editTweetText' WHERE tweets.id = {$_POST['tweet_id']}";
 
         run_mysql_query($query);
@@ -42,11 +45,17 @@
         header("Location: ./account.php");
       }
       elseif(empty($editTweetText) && !empty($editTweetPhoto)) {
-        $query = "UPDATE tweets SET picture = '$editTweetPhoto' WHERE tweets.id = {$_POST['tweet_id']}";
 
-        run_mysql_query($query);
-        $_SESSION['message-success-tweet'] = "Tweet photo has been edited correctly!";
-        header("Location: ./account.php");
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $editTweetPhoto)) {
+          $_SESSION['message-fail-tweet'] = "URL is not valid.";
+          header("Location: ./account.php");
+        } else {
+          $query = "UPDATE tweets SET picture = '$editTweetPhoto' WHERE tweets.id = {$_POST['tweet_id']}";
+
+          run_mysql_query($query);
+          $_SESSION['message-success-tweet'] = "Tweet photo has been edited correctly!";
+          header("Location: ./account.php");
+        }
       }
       else {
         $query = "UPDATE tweets SET picture = '$editTweetPhoto' WHERE tweets.id = {$_POST['tweet_id']}";
@@ -66,14 +75,31 @@
 
     if(!empty($_POST['post_tweet_submit'])) {
 
-      if(empty($_POST['tweet'])) {
+      $postTweet = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['tweet']))));
+      $postTweetPhoto = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['photo']))));
+
+      if(empty($postTweet)) {
         header('Location: ./account.php');
       } else {
-        $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','{$_POST['tweet']}', '{$_POST['photo']}', now())";
+
+        if(!empty($postTweetPhoto)) {
+          if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $postTweetPhoto)) {
+            $_SESSION['message-fail-post'] = "URL is not valid.";
+            header("Location: ./account.php");
+          } else {
+            $_SESSION['message-fail-post'] = "";
+            $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','$postTweet', '$postTweetPhoto', now())";
+
+            run_mysql_query($query);
+            header('Location: ./feed.php');
+          }
+        }
+        $_SESSION['message-fail-post'] = "";
+        $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','$postTweet', '$postTweetPhoto', now())";
 
         run_mysql_query($query);
         header('Location: ./feed.php');
-      }
+      } 
     }
   }
 
@@ -82,21 +108,64 @@
 
     if(!empty($_POST['post_tweet_submit'])) {
 
-      if(empty($_POST['tweet'])) {
+      $postTweetFeed = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['tweet']))));
+      $postPhotoFeed = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['photo']))));
+
+      if(empty($postTweetFeed)) {
         header('Location: ./feed.php');
       } else {
-        $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','{$_POST['tweet']}', '{$_POST['photo']}', now())";
+
+        if(!empty($postPhotoFeed)) {
+          if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $postPhotoFeed)) {
+            // $_SESSION['message-fail-post'] = "URL is not valid.";
+            header("Location: ./feed.php");
+          } else {
+            // $_SESSION['message-fail-post'] = "";
+            $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','$postTweetFeed', '$postPhotoFeed', now())";
+
+            run_mysql_query($query);
+            header('Location: ./feed.php');
+          }
+        }
+        // $_SESSION['message-fail-post'] = "";
+        $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','$postTweetFeed', '$postPhotoFeed', now())";
 
         run_mysql_query($query);
         header('Location: ./feed.php');
-      }
+      } 
     }
   }
+
+  // //post tweet from feed
+  // if(isset($_POST['action']) && $_POST['action'] == 'post_tweet_feed') {
+
+  //   $postTweetFeed = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['tweet']))));
+  //   $postPhotoFeed = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['photo']))));
+
+  //   if(!empty($_POST['post_tweet_submit'])) {
+
+  //     if(empty($postTweetFeed)) {
+  //       header('Location: ./feed.php');
+  //     } else {
+
+  //       if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $postPhotoFeed)) {
+  //         header("Location: ./feed.php");
+  //       } else {
+
+  //         $query = "INSERT INTO tweets (user_id, tweet, picture, created_at) VALUES ('$userid','$postTweetFeed', '$postPhotoFeed', now())";
+
+  //         run_mysql_query($query);
+  //         header('Location: ./feed.php');
+  //       }
+  //     }
+  //   }
+  // }
 
   // Edit photo //
   if(!empty($_POST['edit_photo_submit'])) {
 
-    $newphoto = $_POST['new_photo'];
+    // $newphoto = $_POST['new_photo'];
+    $newphoto = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['new_photo']))));
 
     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $newphoto)) {
       $errors[] = "URL is not valid.";
@@ -118,21 +187,23 @@
   //---- process for changing the first name ----//
   if(isset($_POST['action']) && $_POST['action'] == 'firstname') {
 
-      if(empty($_POST['firstname'])) {
+      $newFirstName = escape_this_string(htmlspecialchars(strip_tags(trim(ucfirst($_POST['firstname'])))));
+
+      if(empty($newFirstName)) {
         $errors[] = "Your name cannot be blank.";
       }
 
-      if (!preg_match("/^[a-zA-Z ]*$/", $_POST['firstname'])) {
+      if (!preg_match("/^[a-zA-Z ]*$/", $newFirstName)) {
         $errors[] = "Only letters and white space allowed for your name.";
       }
 
       if(count($errors) > 0) {
         $_SESSION['errors'] = $errors;
         header("Location: account.php");
-      } elseif ($_POST['firstname'] !== $currentFirstName){
+      } elseif ($newFirstName !== $currentFirstName){
         $_SESSION['errors'] = array();
 
-        $query = "UPDATE users SET firstname = '{$_POST['firstname']}' WHERE id = $userid";
+        $query = "UPDATE users SET firstname = '$newFirstName' WHERE id = $userid";
 
         mysqli_query($connection, $query);
         $_SESSION['message-success'] = "Your first name has been updated correctly!";
@@ -143,12 +214,15 @@
 
   // ---- process for changing the last name ----//
   if(isset($_POST['action']) && $_POST['action'] == 'lastname') {
+
+    $newLastName = escape_this_string(htmlspecialchars(strip_tags(trim(ucfirst($_POST['lastname'])))));
+
     // check if the first name field is empty
-    if(empty($_POST['lastname'])) {
+    if(empty($newLastName)) {
       $errors[] = "The field for updating your last name cannot be blank.";
     }
 
-    if (!preg_match("/^[a-zA-Z ]*$/", $_POST['lastname'])) {
+    if (!preg_match("/^[a-zA-Z ]*$/", $newLastName)) {
       $errors[] = "Only letters and white space allowed for your name";
     }
 
@@ -158,7 +232,7 @@
     } else {
       $_SESSION['errors'] = array();
 
-      $query = "UPDATE users SET lastname = '{$_POST['lastname']}' WHERE id = $userid";
+      $query = "UPDATE users SET lastname = '$newLastName' WHERE id = $userid";
 
       mysqli_query($connection, $query);
       $_SESSION['message-success'] = "Your last name has been updated correctly!";
@@ -169,7 +243,9 @@
 
   //---- process for changing the email ----//
   if(isset($_POST['action']) && $_POST['action'] == 'email') {
-    $email = $_POST['email'];
+
+    $email = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['email']))));
+    // $email = $_POST['email'];
 
     if(empty($email)) {
       $errors[] = "Email field cannot be blank.";
@@ -197,7 +273,8 @@
   //---- process for changing the biography ----//
   if(isset($_POST['action']) && $_POST['action'] == 'bio') {
 
-    $bio = $_POST['biography'];
+    $bio = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['biography']))));
+    // $bio = $_POST['biography'];
 
     // if(empty($bio)) {
     //   $bio = fetch_record("SELECT bio FROM users WHERE username = '$username'")['bio'];
@@ -217,9 +294,8 @@
 
   //---- process for changing the country ----//
   if(isset($_POST['action']) && $_POST['action'] == 'country') {
-    $country = escape_this_string($_POST['country']);
 
-    // $country = htmlspecialchars(strip_tags(trim($country)));
+    $country = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['country']))));
 
     $query = "UPDATE users SET country = '$country' WHERE id = $userid";
 
