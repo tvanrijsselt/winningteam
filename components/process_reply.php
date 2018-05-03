e3481974c866388f07966277a629f368b182416f
FemkeLeentje <femkelb@gmail.com>
2018-05-03 18:14:45 +0200

    fixed bug in replying

--- components/process_reply.php
+++ components/process_reply.php
@@ -9,7 +9,7 @@ $userid = $_SESSION['user-id']; //change to logged in userid
 $tweet_id = $_POST['tweet_id'];
 
 $content = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['content']))));
-$content = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['picture']))));
+$picture = escape_this_string(htmlspecialchars(strip_tags(trim($_POST['picture']))));
 
 if ($content != '' OR $picture != '') {
     run_mysql_query("INSERT INTO replies (content, picture, user_id, tweet_id, created_at) 
